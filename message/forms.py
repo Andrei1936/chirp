d0be9bb79843593b9252755e406498c799a49ebc
Andrei1936 <andreichirciu@gmail.com>
2017-07-25 11:09:10 +0300

    a lot of changes
    asdasd

--- message/forms.py
+++ message/forms.py
@@ -1,7 +1,8 @@
+from builtins import ValueError
+
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-
 
 class RegisterForm(forms.ModelForm):
     class Meta:
@@ -16,7 +17,7 @@ class RegisterForm(forms.ModelForm):
         password = self.cleaned_data['password']
         retype_password = self.cleaned_data['retype_password']
         if password != retype_password:
-            raise ValidationError("The two passwords do not match")
+            raise ValidationError("The two passwords do not match!")
         return retype_password
 
     def save(self, commit=True):
@@ -25,3 +26,7 @@ class RegisterForm(forms.ModelForm):
         if commit:
             new_user.save()
         return new_user
+
+    class MessageForm(forms.ModelForm):
+        class Meta:
+            model = Message
