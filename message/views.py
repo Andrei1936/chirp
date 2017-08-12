777f26a21fd2e4ebc7cd31c062e824ae289f6e8c
Andrei1936 <andreichirciu@gmail.com>
2017-08-12 17:32:32 +0300

    asdsfsdf

--- message/views.py
+++ message/views.py
@@ -1,9 +1,9 @@
-# Create your views here.
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.shortcuts import render
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.utils import IntegrityError
+from django.views.decorators.csrf import csrf_exempt
 
 from django.views.generic.base import View
 from django.views.generic.detail import DetailView
@@ -11,7 +11,7 @@ from django.views.generic.edit import CreateView, FormView
 from django.views.generic.list import ListView
 
 from message.forms import RegisterForm, MessageForm
-from message.models import Message, Follow
+from message.models import Message, Follow, Like
 
 
 class RegisterView(CreateView):
@@ -23,10 +23,10 @@ class TimelineView(ListView):
     template_name = 'index.html'
     def get_queryset(self):
         #return Message.objects.all().order_by("-created")
-        # implement the logic
+        #implement the logic
         if self.request.user.is_authenticated:
-            return Message.objects.filter(user=self.request.user).order_by("-created")
-        else:
+            #return Message.objects.filter(user=self.request.user).order_by("-created")
+        #else:
             return Message.objects.all().order_by("-created")
 
 class ProfileBaseView(DetailView):
@@ -46,11 +46,9 @@ class MyProfileView(ProfileBaseView):
     def get_object(self, queryset=None):
         return self.request.user
 
-
 class ProfileView(ProfileBaseView):
     def get_slug_field(self):
         return "username"
-
 
 def follow_user(request, username):
     user = get_object_or_404(User, username=username)
@@ -67,15 +65,34 @@ def unfollow_user(request, username):
     try:
         follow = Follow.objects.filter(followed_user=user, following_user=request.user).first()
         follow.delete()
-        messages.info(request, "You are now no longer following {0}".format(username))
+        messages.info(request, "You are no longer following {0}".format(username))
     except IntegrityError:
         messages.error(request, "You are already following this user")
     return redirect('profile', username)
-
 
 def new_chirp(request):
     if request.method == "POST":
         form = MessageForm(data=request.POST)
         if form.is_valid():
             form.save()
-        return redirect("index")
+    return redirect("index")
+
+@csrf_exempt
+def like_message(request):
+    if request.method == "POST":
+        message_id = request.POST.get('id')
+        print(message_id)
+        like_value = bool(int(request.POST.get('like')))
+        print(like_value)
+        message = get_object_or_404(Message, id=message_id)
+        try:
+            like = Like.objects.get(user=request.user, message=message)
+            if like.like == like_value:
+                like.delete()
+            else:
+                like.like = like_value
+                like.save()
+        except Like.DoesNotExist:
+            like = Like(user=request.user, message=message, like=like_value)
+            like.save()
+    return JsonResponse({'succes':'true'})
---
+++ static/js/likeDislike.js
@@ -0,0 +1,50 @@
+(function(){
+    'use strict';
+    $(document).ready(function(){
+        $('.like-message').click(function(){
+            /*alert("Pressed a like button!");*/
+            var request = $.ajax({
+                url: "http://127.0.0.1:8000/like/",
+                method: "POST",
+                data: {
+                    id : $(this).attr("data-messageid"),
+                    like: 1
+                }
+            });
+
+            request.done(function(){
+               /*alert("SUCCES!");*/
+               location.reload();
+
+            });
+
+            request.fail(function(){
+                /*alert("FAIL!");*/
+                location.reload();
+            });
+
+        });
+
+        $('.dislike-message').click(function(){
+            /*alert("Pressed a dislike button!");*/
+            var request = $.ajax({
+                url: "http://127.0.0.1:8000/like/",
+                method: "POST",
+                data: {
+                    id : $(this).attr("data-messageid"),
+                    like: 0
+                }
+            });
+
+            request.done(function(){
+               /*alert("SUCCES!");*/
+               location.reload();
+            });
+
+            request.fail(function(){
+                /*alert("FAIL!");*/
+                location.reload();
+            });
+        });
+    });
+})();
